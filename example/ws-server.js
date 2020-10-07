98b351ba8a511638d35d7ae35be41b41b2ac1f5e
Ulric Wilfred <shaman.sir@gmail.com>
2020-10-07 14:26:27 +0200

    collect and send updates to the spawned interfaces

--- example/ws-server.js
+++ example/ws-server.js
@@ -2,19 +2,27 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3333 });
 
-const data = {};
+const updates = {};
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     let package = JSON.parse(message);
     console.log('uuid', package.uuid);
+
     console.log('code', package.code);
-    if (package.code == 'ASK') {
-        broadcast(ws, message);
+    if (package.code == 'ACK') {
+        //broadcast(ws, package.uuid, message);
+        if (updates[package.uuid]) {
+            updates[package.uuid].forEach(function(update) {
+                ws.send(update);
+            });
+        }
     } else if (package.code == 'BLD') {
-        broadcast(ws, message);
+        broadcast(ws, package.uuid, message);
     } else if (package.code == 'UPD') {
-        broadcast(ws, message);
+        if (!updates[package.uuid]) { updates[package.uuid] = []; }
+        updates[package.uuid].push(message);
+        broadcast(ws, package.uuid, message);
     }
     console.log('received: %s', message);
   });
@@ -23,7 +31,7 @@ wss.on('connection', function connection(ws) {
 });
 
 
-function broadcast(from, data) {
+function broadcast(from, uuid, data) {
     wss.clients.forEach(function each(client) {
         if (client !== from && client.readyState === WebSocket.OPEN) {
             client.send(data);
