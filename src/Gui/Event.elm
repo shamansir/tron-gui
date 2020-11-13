de355a887abde052af133d8602334e038e1b08dd
Ulric Wilfred <shaman.sir@gmail.com>
2020-11-13 21:44:12 +0100

    the draft for `dispatch` function, old good `Event.dispatch`

--- src/Gui/Event.elm
+++ src/Gui/Event.elm
@@ -1,12 +1,25 @@
 module Gui.Event exposing (..)
 
 
+import Gui.Path exposing (Path)
+import Gui.Property exposing (Property)
+
+
 type Event
     = Click
     | DragStart ( Float, Float )
     | Drag ( Float, Float )
     | DragEnd ( Float, Float )
-    | Focus
-    | LoseFocus
+    --| Focus
     | Key Int
     | Batch (List Event)
+
+
+type alias Updates msg = ( List ( Path, Property msg ), Cmd msg )
+
+
+-- TODO: move `Property.execute`, `Property.executeAt`, `Gui.handleKeyDown`, `Gui.handleMouse` here
+
+
+dispatch : Event -> Property msg -> Path -> Property msg -> Updates msg
+dispatch event root path prop = ( [], Cmd.none )
