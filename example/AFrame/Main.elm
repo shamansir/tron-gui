3b302a5476bd555af9b3bf24c27edbd7ae14ab8f
Ulric Wilfred <shaman.sir@gmail.com>
2020-11-13 21:59:49 +0100

    the draft `a-frame` example

---
+++ example/AFrame/Main.elm
@@ -0,0 +1,93 @@
+module AFrame.Main exposing (main)
+
+
+import Browser exposing (element)
+import Html exposing (Html, div)
+import Html.Attributes as Attr exposing (class)
+
+import Gui as Tron exposing (Gui, init, view, update, subscriptions)
+import Gui.Msg as Tron exposing (Msg(..))
+import Gui.Style.Theme as Tron exposing (Theme)
+
+import Default.Main as Default
+import Default.Model as Default
+import Default.Msg as Default
+import Default.Gui as DefaultGui
+
+
+type Msg
+    = ToDefault Default.Msg
+    | ToTron Tron.Msg
+
+
+type alias Example
+    = Default.Model
+
+
+type alias Model =
+    ( Example, Tron.Gui Msg )
+
+
+init : flags -> ( Model, Cmd Msg )
+init _ =
+    let
+        example = Default.init
+        ( gui, guiEffect ) =
+            DefaultGui.for example
+                |> Tron.init
+    in
+        (
+            ( example
+            , gui |> Tron.map ToDefault
+            )
+        , guiEffect |> Cmd.map ToTron
+        )
+
+
+view : Model -> Html Msg
+view ( example, gui ) =
+    Html.div
+        [ ]
+        [ gui
+            |> Tron.view Tron.light
+            |> Html.map ToTron
+        , Default.view example
+        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg ( example, gui ) =
+    case msg of
+
+        ToDefault dmsg ->
+            (
+                ( example |> Default.update dmsg
+                , gui
+                )
+            , Cmd.none
+            )
+
+        ToTron guiMsg ->
+            case gui |> Tron.update guiMsg of
+                ( nextGui, cmds ) ->
+                    (
+                        ( example
+                        , nextGui
+                        )
+                    , cmds
+                    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions ( _, gui ) =
+    Tron.subscriptions gui |> Sub.map ToTron
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , subscriptions = subscriptions
+        , update = update
+        }
---
+++ example/AFrame/Render/Layout.elm
@@ -0,0 +1,8 @@
+module AFrame.Render.Layout exposing (..)
+
+import Gui exposing (Gui)
+
+import Html exposing (Html, div)
+
+view : Gui msg -> Html msg
+view _ = div [] []
