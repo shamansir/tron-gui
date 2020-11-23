3a55ebf1b436ac4d073c34c2d66558e322fd2e5a
Ulric Wilfred <shaman.sir@gmail.com>
2020-11-23 11:04:14 +0100

    add messages to the `Goose` example

--- example/Example/Goose/Model.elm
+++ example/Example/Goose/Model.elm
@@ -1,4 +1,73 @@
 module Example.Goose.Model exposing (..)
 
 
-type alias Model = ()
+import Color exposing (Color)
+
+
+type alias Model =
+    { honk : ( Bool, HonkConfig )
+    , eye : EyeConfig
+    , lookAt : LookDirection
+    , shoes : Shoes
+    , punk : Bool
+    , colors : Colors
+    }
+
+
+init : Model
+init =
+    { honk =
+        ( True
+        ,
+            { text = "CAI!"
+            , size = 36
+            , position = ( 0, 0 )
+            , color = Color.black
+            }
+        )
+    , eye =
+        EyeConfig ( 0, 0 ) 5
+    , lookAt = Left
+    , shoes = Original
+    , punk = False
+    , colors =
+        { feathers = Color.rgb255 236 41 123
+        , skin = Color.rgb255 253 182 13
+        , eye = Color.black
+        , background = Color.white
+        , iroquois = Color.black
+        }
+    }
+
+
+type alias HonkConfig =
+    { text : String
+    , size : Int
+    , position : ( Float, Float )
+    , color : Color
+    }
+
+
+type alias EyeConfig =
+    { position : ( Float, Float )
+    , size : Float
+    }
+
+
+type LookDirection
+    = Left
+    | Right
+
+
+type Shoes
+    = Original
+    | Boots
+
+
+type alias Colors =
+    { feathers : Color
+    , skin : Color
+    , eye : Color
+    , background : Color
+    , iroquois : Color
+    }
--- example/Example/Goose/Msg.elm
+++ example/Example/Goose/Msg.elm
@@ -1,4 +1,24 @@
 module Example.Goose.Msg exposing (..)
 
 
-type alias Msg = ()
+import Color exposing (Color)
+import Example.Goose.Model exposing (LookDirection, Shoes)
+
+
+type Msg
+    = HonkOn
+    | HonkOff
+    | PunkOn
+    | PunkOff
+    | ChangeHonkText String
+    | ChangeHonkTextSize Int
+    | ChangeHonkTextPosition ( Float, Float )
+    | ChangeHonkTextColor Color
+    | ChangeEyePosition ( Float, Float )
+    | ChangeEyeSize Float
+    | LookAt LookDirection
+    | ChangeShoes Shoes
+    | ChangeFeathersColor Color
+    | ChangeSkinColor Color
+    | ChangeEyeColor Color
+    | ChangeBackground Color
