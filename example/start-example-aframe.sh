05f7bf16a92326e377588d1a589c53b54e0ca42d
Ulric Wilfred <shaman.sir@gmail.com>
2020-12-17 13:30:02 +0100

    change port for some examples

--- example/start-example-aframe.sh
+++ example/start-example-aframe.sh
@@ -1,3 +1,3 @@
 cp ./Example/Goose/JetBrainsMono-Regular.woff ./JetBrainsMono-Regular.woff
 cp ./Example/Goose/JetBrainsMono-Regular.woff2 ./JetBrainsMono-Regular.woff2
-elm-live ./AFrame/Main.elm --open --dir=. --start-page=./AFrame/index.html -- --output=./app.js
+elm-live --port 8100 ./AFrame/Main.elm --open --dir=. --start-page=./AFrame/index.html -- --output=./app.js
