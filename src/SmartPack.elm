33076de69ffbde6a4a5df61c05ee2d4b7b0908a2
Ulric Wilfred <shaman.sir@gmail.com>
2021-04-01 16:41:46 +0200

    use the external matrix package to store the layout

--- src/SmartPack.elm
+++ src/SmartPack.elm
@@ -2,27 +2,22 @@ module SmartPack exposing (..)
 
 
 import Array exposing (Array)
-
+import Matrix exposing (Matrix)
 
 import BinPack exposing (Bounds)
 import Size exposing (..)
 
 
+type Distribution
+    = Up
+    | Down
+    | Right
+    | Left
+    | UpAndDown
+    | LeftAndRight
+
+
 type SmartPack a = SmartPack (Size Cells) (List (Bounds, a))
-
-
-toMatrix : SmartPack a -> Array (Array ( Int, Maybe a ))
-toMatrix (SmartPack (Size (w, h)) items) =
-    let
-        addBounds bounds matrix =
-            matrix
-    in items
-        |> List.sortBy (Tuple.first >> .x)
-        |> List.foldl
-            (\(bounds, v) ->
-                addBounds bounds
-            )
-            (Array.repeat w (Array.repeat h ( 0, Nothing )))
 
 
 container : Size Cells -> SmartPack a
@@ -30,11 +25,31 @@ container size = SmartPack size []
 
 
 pack : SizeF Cells -> a -> SmartPack a -> Maybe (SmartPack a)
-pack _ _ s = Just s
+pack size v s =
+    s
+        |> findSpot size
+        |> Maybe.map
+            (\(x, y) ->
+                case ( s, size ) of
+                    ( SmartPack ps xs, SizeF ( w, h ) ) ->
+                        SmartPack ps
+                            <| ( { x = x, y = y, width = w, height = h }, v ) :: xs
+
+            )
 
 
 packAt : ( Float, Float ) -> SizeF Cells -> a -> SmartPack a -> Maybe (SmartPack a)
 packAt _ _ _ s = Just s
+
+
+packCloseTo
+    :  ( Float, Float )
+    -> SizeF Cells
+    -> Distribution
+    -> a
+    -> SmartPack a
+    -> Maybe (SmartPack a)
+packCloseTo _ _ _ _ s = Just s
 
 
 carelessPack : SizeF Cells -> a -> SmartPack a -> SmartPack a
@@ -45,9 +60,49 @@ carelessPackAt : ( Float, Float ) -> SizeF Cells -> a -> SmartPack a -> SmartPac
 carelessPackAt _ _ _ s = s
 
 
-asList : SmartPack a -> List (Bounds, a)
-asList (SmartPack _ items) = items
+carelessPackCloseTo
+    :  ( Float, Float )
+    -> SizeF Cells
+    -> Distribution
+    -> a
+    -> SmartPack a
+    -> SmartPack a
+carelessPackCloseTo _ _ _ _ s = s
+
+
+toList : SmartPack a -> List (Bounds, a)
+toList (SmartPack _ items) = items
 
 
 dimensions : SmartPack a -> Size Cells
 dimensions (SmartPack size _) = size
+
+
+toMatrix : SmartPack a -> Matrix ( Maybe a )
+toMatrix (SmartPack (Size (w, h)) items) =
+    let
+        addBounds bounds matrix =
+            matrix
+    in items
+        |> List.sortBy (Tuple.first >> .x)
+        |> List.foldl
+            (\(bounds, v) ->
+                addBounds bounds
+            )
+            (Matrix.initialize (w, h) <| always Nothing)
+
+
+findSpot : SizeF Cells -> SmartPack a -> Maybe ( Float, Float )
+findSpot size = toMatrix >> findSpotM size
+
+
+findSpotM : SizeF Cells -> Matrix (Maybe a) -> Maybe ( Float, Float )
+findSpotM _ _ = Just ( 0, 0 )
+
+
+find : ( Float, Float ) -> SmartPack a -> Maybe a
+find pos = toMatrix >> findM pos
+
+
+findM : ( Float, Float ) -> Matrix ( Maybe a ) -> Maybe a
+findM pos _ = Nothing
