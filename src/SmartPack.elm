560e4107c5a62e90743ba615e71401eb27627b43
Ulric Wilfred <shaman.sir@gmail.com>
2021-04-03 12:58:27 +0200

    preparations for finding spot algorithm

--- src/SmartPack.elm
+++ src/SmartPack.elm
@@ -20,20 +20,30 @@ type Distribution
 type SmartPack a = SmartPack (Size Cells) (List (Bounds, a))
 
 
+map : (a -> b) -> SmartPack a -> SmartPack b
+map f (SmartPack size list) =
+    SmartPack size <| List.map (Tuple.mapSecond f) <| list
+
+
+withBounds : (Bounds -> Bounds) -> SmartPack a -> SmartPack a
+withBounds f (SmartPack size list) =
+    SmartPack size <| List.map (Tuple.mapFirst f) <| list
+
+
 container : Size Cells -> SmartPack a
 container size = SmartPack size []
 
 
-pack : SizeF Cells -> a -> SmartPack a -> Maybe (SmartPack a)
-pack size v s =
+pack : Distribution -> Size Cells -> a -> SmartPack a -> Maybe (SmartPack a)
+pack distribution size v s =
     s
-        |> findSpot size
+        |> findSpot distribution size
         |> Maybe.map
             (\(x, y) ->
                 case ( s, size ) of
-                    ( SmartPack ps xs, SizeF ( w, h ) ) ->
+                    ( SmartPack ps xs, Size ( w, h ) ) ->
                         SmartPack ps
-                            <| ( { x = x, y = y, width = w, height = h }, v ) :: xs
+                            <| ( { x = toFloat x, y = toFloat y, width = toFloat w, height = toFloat h }, v ) :: xs
 
             )
 
@@ -43,17 +53,17 @@ packAt _ _ _ s = Just s
 
 
 packCloseTo
-    :  ( Float, Float )
+    :  Distribution
+    -> ( Float, Float )
     -> SizeF Cells
-    -> Distribution
     -> a
     -> SmartPack a
     -> Maybe (SmartPack a)
 packCloseTo _ _ _ _ s = Just s
 
 
-carelessPack : SizeF Cells -> a -> SmartPack a -> SmartPack a
-carelessPack _ _ s = s
+carelessPack : Distribution -> SizeF Cells -> a -> SmartPack a -> SmartPack a
+carelessPack _ _ _ s = s
 
 
 carelessPackAt : ( Float, Float ) -> SizeF Cells -> a -> SmartPack a -> SmartPack a
@@ -92,12 +102,47 @@ toMatrix (SmartPack (Size (w, h)) items) =
             (Matrix.initialize (w, h) <| always Nothing)
 
 
-findSpot : SizeF Cells -> SmartPack a -> Maybe ( Float, Float )
-findSpot size = toMatrix >> findSpotM size
+findSpot : Distribution -> Size Cells -> SmartPack a -> Maybe ( Int, Int )
+findSpot distribution size = toMatrix >> findSpotM distribution size
 
 
-findSpotM : SizeF Cells -> Matrix (Maybe a) -> Maybe ( Float, Float )
-findSpotM _ _ = Just ( 0, 0 )
+findSpotM : Distribution -> Size Cells -> Matrix (Maybe a) -> Maybe ( Int, Int )
+findSpotM distribution (Size (wc, hc)) matrix =
+    let
+
+        isEmpty cell =
+            case cell of
+                Just _ -> False
+                Nothing -> True
+
+        fitsAt (x, y) =
+            matrix
+                |> Matrix.slice (x, y) (x + wc, y + hc)
+                |> foldM (Tuple.second >> isEmpty >> (&&)) True
+
+        firstPos =
+            (0, 0)
+
+        maybeNext (x, y) =
+            Nothing
+
+        helper (x, y) =
+            if fitsAt (x, y) then
+                Just (x, y)
+            else
+                maybeNext (x, y)
+                    |> Maybe.andThen helper
+
+    in
+        if (wc > 0) && (hc > 0)
+            && not (Matrix.isEmpty matrix)
+            then
+                case Matrix.size matrix of
+                    ( mw, mh ) ->
+                        if (mw > wc) && (mh > hc)
+                            then helper firstPos
+                            else Nothing
+        else Nothing
 
 
 find : ( Float, Float ) -> SmartPack a -> Maybe a
@@ -106,3 +151,14 @@ find pos = toMatrix >> findM pos
 
 findM : ( Float, Float ) -> Matrix ( Maybe a ) -> Maybe a
 findM pos _ = Nothing
+
+
+fold : (((Int, Int), Maybe a) -> b -> b) -> b -> SmartPack a -> b
+fold f init =
+    toMatrix >> foldM f init
+
+
+foldM : (((Int, Int), Maybe a) -> b -> b) -> b -> Matrix (Maybe a) -> b
+foldM f init =
+    Matrix.toIndexedList
+        >> List.foldl f init
