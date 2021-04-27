425ce7ed03fcc459935066cad8a8aa71178cd371
egor.alekseev <egor.alekseev@jetbrains.com>
2021-04-27 16:59:35 +0300

    add more controls

--- example/Example/Tiler/Gui.elm
+++ example/Example/Tiler/Gui.elm
@@ -1,10 +1,12 @@
 module Example.Tiler.Gui exposing (gui)
 
+import Color
 import Tron exposing (Tron)
 import Tron.Builder.Unit as Tron
 import Tron.Style.PanelShape exposing (..)
 import Tron.Style.CellShape as CellShape exposing (..)
 import Tron.Style.Theme as Theme
+
 
 
 gui : Tron ()
@@ -14,7 +16,7 @@ gui =
         , ( "Sizes", sizes |> Tron.setIcon (icon "size") )
         , ( "Tile", tile |> Tron.setIcon (icon "tile") )
         , ( "Randomness", randomness |> Tron.setIcon (icon "settings") )
-        , ( "Title", Tron.none )
+        , ( "Title", title |> Tron.setIcon(icon "text") )
         , ( "Action on click", clickAction |> Tron.setIcon (icon "cursor"))
         , ( "Shuffle color", Tron.buttonWith <| icon "shuffle" )
         , ( "Shuffle tiles", Tron.buttonWith <| icon "shuffle")
@@ -41,7 +43,7 @@ icon name =
 sizes : Tron ()
 sizes =
      Tron.nest
-        (cols 2)
+        (cols 3)
         CellShape.single
         [ ( "Cell", Tron.int { min = 0, max = 200, step = 1 } 0 )
         , ( "Shape", Tron.float { min = 0.01, max = 5, step = 0.1 } 0 )
@@ -67,14 +69,29 @@ tile =
         (cols 2)
         CellShape.single
         [ ("Tileset", tileset)
-        , ( "Opacity", Tron.float { min = 0.1, max = 2, step = 0.1 } 0.1 )
+        , ( "Fill", Tron.int { min = 0, max = 255, step = 1 } 178 )
+        , ( "Opacity", Tron.int { min = 0, max = 255, step = 1 } 255 )
+        , ( "Stroke", Tron.int { min = 0, max = 10, step = 1 } 1 )
         ]
 
 
 tileset : Tron ()
 tileset =
         Tron.strings
-        [ "Foo", "Bar", "F44oo"] "Foo"
+        [ "Geometric", "Fancy", "Lines"] "Foo"
+
+
+title : Tron()
+title =
+    Tron.nest
+        (cols 3)
+        CellShape.single
+        [ ("Show", Tron.toggle False)
+        , ( "Font size", Tron.int { min = 0, max = 72, step = 1 } 16 )
+        , ( "Opacity", Tron.int { min = 0, max = 255, step = 1 } 255 )
+        , ("Position", Tron.xy ( { min = -20, max = 20, step = 5 }, { min = -20, max = 20, step = 5 } ) (0, 0))
+        , ( "Color",  Tron.color <| Color.rgb255 255 194 0)
+        ]
 
 
 type ActionType
