7bdeccb352b10821e6f26fcbf667c0b274b3b150
egor.alekseev <egor.alekseev@jetbrains.com>
2021-04-27 15:35:40 +0300

    separate tiler

---
+++ example/ForTiler/Main.elm
@@ -0,0 +1,26 @@
+port module ForTiler.Main exposing (..)
+
+
+import Tron.Style.Theme as Theme
+import Tron.Style.Dock as Dock
+import Tron.Option as Option
+import Tron.Expose.Data as Exp
+import WithTron exposing (BackedWithTron)
+
+
+import Example.Tiler.Gui as ExampleGui
+
+
+port ack : Exp.RawProperty -> Cmd msg
+
+port transmit : Exp.RawOutUpdate -> Cmd msg
+
+
+main : BackedWithTron
+main =
+    WithTron.backed
+        (Option.toHtml Dock.bottomCenter Theme.dark)
+        ( ack, transmit )
+        ExampleGui.gui
+
+
