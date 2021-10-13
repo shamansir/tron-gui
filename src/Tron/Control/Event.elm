e41700566ef9324aa985802955890eaf0b1ecf0f
Ulric Wilfred <shaman.sir@gmail.com>
2021-10-13 14:34:33 +0200

    the draft for the actions to be sent to controls

--- src/Tron/Control/Event.elm
+++ src/Tron/Control/Event.elm
@@ -1,12 +1,17 @@
 module Tron.Control.Event exposing (..)
 
 
+import Tron.Mouse as Mouse
 import Tron.Pages as Pages
 
 
-type Event
-    = Click
-    | MouseDown
+type Action
+    = Execute -- i.e. Click / Press Enter
+    | Focus
+    | DragStart { x : Float, y : Float }
+    | Dragging { dx : Float, dy : Float }
+    | DragFinish { dx : Float, dy : Float }
     | KeyDown Int
+    | Select Int
     | TextInput String
     | SwitchPage Pages.PageNum
