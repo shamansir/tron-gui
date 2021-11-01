e9282cab6f6d3d2823e4420b1b9e655a336c952f
Ulric Wilfred <shaman.sir@gmail.com>
2021-11-01 13:23:42 +0100

    `BuildFromJs`: implement `Control` and `Nest` methods

--- example/BuildFromJs/builder.ts
+++ example/BuildFromJs/builder.ts
@@ -27,24 +27,66 @@ interface SendValuePort {
 }
 
 
-class Control {
-  readonly companionProperty? : string = undefined;
-  readonly id? : string = undefined;
-  readonly type : ControlType = "none";
-  changeHandlers : ChangeHandler[] = [];
-  finishChangeHandlers : ChangeHandler[] = [];
-  onChange(handler : ChangeHandler) : Control { return this; };
-  onFinishChange(handler : ChangeHandler) : Control { return this; };
-  sendChange(value: Value) : Control { return this; }
+export class Control {
+    readonly companion? : any;
+    readonly companionProperty? : string;
+    label? : string;
+    readonly type : ControlType;
+    readonly props : any;
+    //readonly path : string[];
+
+    constructor(type: ControlType, props : any, companion? : any, companionProperty?: string, label?: string) {
+        this.type = type;
+        this.props = props;
+        this.companion = companion;
+        this.companionProperty = companionProperty;
+        this.label = label || companionProperty;
+    }
+
+    name(name: string): Control { this.label = name; return this; }
+
+    changeHandlers : ChangeHandler[] = [];
+    //finishChangeHandlers : ChangeHandler[] = [];
+
+    onChange(handler : ChangeHandler) : Control {
+            this.changeHandlers.push(handler);
+            return this;
+        };
+    //onFinishChange(handler : ChangeHandler) : Control { return this; };
+    sendChange(value: Value) : Control {
+            for (const changeHandler in this.changeHandlers) {
+                this.changeHandlers[changeHandler](value);
+            }
+            return this;
+        }
+
+    toJson(): any { return {}; }
 }
 
-class Nest extends Control {
-  controls : Control[] = [];
-  addOne(control : Control) : Nest { return this; }
-  add(controls : Control[]) : Nest { return this; }
+
+export class Nest extends Control {
+    controls : Control[] = [];
+
+    constructor(label : string, companion? : any, companionProperty?: string) {
+        super("nest", {}, companion, companionProperty, label);
+    }
+
+    add(control : Control) : Control
+        {
+            this.controls.push(control);
+            return this;
+        };
+    addMany(controls : Control[]) : Control
+        {
+            this.controls.concat(controls);
+            return this;
+        };
+
+    num(companion : any, companionPropety : string, min : number = 0, max : number = 100, step : number = 1) : Control
+        { return this.add(new Control("num", { min, max, step }, companion, companionPropety)); };
+    button(companion : any, companionPropety : string) : Control
+        { return this.add(new Control("button", { }, companion, companionPropety)); };
 }
-
-
 
 
 export class Ports {
@@ -54,12 +96,11 @@ export class Ports {
 }
 
 
-export type Tron =
-    { ports : Ports
-    , root : Nest
-    };
+export class Tron extends Nest {
+    constructor() { super('root'); }
+}
 
 
-export function build(ports : Ports, root : Nest): Tron {
-    return { ports, root };
+export function build(ports : Ports, root : Tron): Tron {
+    return root;
 }
--- example/BuildFromJs/example.ts
+++ example/BuildFromJs/example.ts
@@ -1,5 +1,15 @@
-import { Ports } from "./builder.js";
+import { Nest, Tron, Ports, build } from "./builder.js";
 
 export function buildExample(ports : Ports) : void {
 
+    const companion =
+        { value : 42
+        , test : () => { console.log('test'); }
+        };
+
+    const tron : Tron = new Tron();
+    tron.num(companion, 'value', 0, 42).onChange((val) => { console.log(companion.value, val); });
+    tron.button(companion, 'test');
+
+    build(ports, tron);
 }
